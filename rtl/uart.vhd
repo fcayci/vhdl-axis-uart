e9f68324e826dc5a70b19618d1007c14301ece48
Furkan Cayci <furkanca02@gmail.com>
2022-10-19 23:57:15 +0100

    Remove component declarations
    It takes so much space. It requires manual listing of files (to be in
    order) but this way it is cleaner.

--- rtl/uart.vhd
+++ rtl/uart.vhd
@@ -31,53 +31,15 @@ entity uart is
 end uart;
 
 architecture rtl of uart is
-    component uart_rx is
-    generic (
-        CLKFREQ    : integer := 125E6;
-        BAUDRATE   : integer := 115200;
-        DATA_WIDTH : integer := 8;
-        PARITY     : string  := "NONE"; -- NONE, EVEN, ODD
-        STOP_WIDTH : integer := 1
-    );
-    port (
-        clk     : in  std_logic;
-        -- external interface signals
-        rxd     : in  std_logic;
-        -- axi stream interface
-        m_axis_tready : in  std_logic;
-        m_axis_tdata  : out std_logic_vector(DATA_WIDTH-1 downto 0);
-        m_axis_tvalid : out std_logic
-    );
-    end component;
-
-    component uart_tx is
-    generic (
-        CLKFREQ    : integer := 125E6;
-        BAUDRATE   : integer := 115200;
-        DATA_WIDTH : integer := 8;
-        PARITY     : string  := "NONE"; -- NONE, EVEN, ODD
-        STOP_WIDTH : integer := 1
-    );
-    port (
-        clk     : in  std_logic;
-        -- external interface signals
-        txd     : out std_logic;
-        -- axi stream interface
-        s_axis_tvalid : in  std_logic;
-        s_axis_tdata  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
-        s_axis_tready : out std_logic
-    );
-    end component;
-
 begin
 
-    urx : uart_rx
+    urx : entity work.uart_rx(rtl)
         generic map (CLKFREQ=>CLKFREQ, BAUDRATE=>BAUDRATE,
                      DATA_WIDTH=>DATA_WIDTH, PARITY=>PARITY, STOP_WIDTH=>STOP_WIDTH)
         port map (clk=>clk, rxd=>rxd, m_axis_tready=>m_axis_tready,
                   m_axis_tdata=>m_axis_tdata, m_axis_tvalid=>m_axis_tvalid);
 
-    utx : uart_tx
+    utx : entity work.uart_tx(rtl)
         generic map (CLKFREQ=>CLKFREQ, BAUDRATE=>BAUDRATE,
                      DATA_WIDTH=>DATA_WIDTH, PARITY=>PARITY, STOP_WIDTH=>STOP_WIDTH)
         port map (clk=>clk, txd=>txd, s_axis_tvalid=>s_axis_tvalid,
